0b47eba278ea76f5d8f1329f911e67021c8c194f
jwilkins48 <jwilkinsdev@gmail.com>
2026-05-28 18:29:40 -0500

    temporary admin access

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,6 +4,7 @@ import {
   validateBody,
   validateRegister,
 } from "../middleware/validationMiddleware.js";
+import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
 
@@ -12,5 +13,19 @@ router.post("/register", validateBody, validateRegister, register);
 
 // Login user - /api/auth/login
 router.post("/login", validateBody, login);
+
+// TEMPORARY (for deployment)
+router.post("/make-admin", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await prisma.user.update({
+      where: { email },
+      data: { isAdmin: true },
+    });
+    res.json({ message: `${user.email} is now an admin` });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 export default router;
