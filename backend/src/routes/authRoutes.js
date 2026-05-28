83b7a63bc83c4416c9aeac4bd9b7415206eb124c
jwilkins48 <jwilkinsdev@gmail.com>
2026-05-28 18:41:22 -0500

    Remove temporary admin route

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,29 +4,13 @@ import {
   validateBody,
   validateRegister,
 } from "../middleware/validationMiddleware.js";
-import { PrismaClient } from "@prisma/client";
 
 const router = express.Router();
-const prisma = new PrismaClient();
 
 // Create new user - /api/auth/register
 router.post("/register", validateBody, validateRegister, register);
 
 // Login user - /api/auth/login
 router.post("/login", validateBody, login);
-
-// TEMPORARY (For Deployment)
-router.post("/make-admin", async (req, res) => {
-  try {
-    const { email } = req.body;
-    const user = await prisma.user.update({
-      where: { email },
-      data: { isAdmin: true },
-    });
-    res.json({ message: `${user.email} is now an admin` });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
 
 export default router;
